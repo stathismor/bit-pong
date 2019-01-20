1b108d555f32a3d406e427b0c7cc790d86c171e5
Stathis Moraitidis <emoraitidis@gmail.com>
2019-01-20 16:21:47 +0000

    Use velocity to throw

--- src/sprite/Ball.js
+++ src/sprite/Ball.js
@@ -1,13 +1,39 @@
+const _SPEED = 0.10;
+const _STIFFNESS = 0.0001;
+
+
 class Ball extends Phaser.Physics.Matter.Sprite {
   constructor(scene, x, y, key) {
-    super(scene.matter.world, x, y, key, null, { isStatic: true });
+    super(scene.matter.world, x, y, key, null);
+    this.startPos = { x, y, };
+    this.spring = scene.matter.add.mouseSpring();
 
+    this.setCircle();
+    this.setStatic(true);
     this.setInteractive({ draggable: true });
     scene.input.setDraggable(this);
-    scene.matter.add.mouseSpring();
+
+    this.constraint = Phaser.Physics.Matter.Matter.Constraint.create({
+      pointA: {x, y,},
+      bodyB: this.body,
+      stiffness: _STIFFNESS,
+      damping: 1,
+    });
 
     scene.input.on('dragstart', (pointer, gameObject) => {
       gameObject.setStatic(false);
+    });
+
+    scene.input.on('dragend', function(pointer, gameObject) {
+      gameObject.setStatic(false);
+      gameObject.setVelocity(
+        (gameObject.startPos.x - gameObject.x) * _SPEED,
+        (gameObject.startPos.y - gameObject.y) * _SPEED
+      );
+
+      gameObject.scene.matter.world.removeConstraint(gameObject.constraint);
+      gameObject.spring.destroy();
+
     });
   }
 }
