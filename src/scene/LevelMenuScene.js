a425cc8cb0ff2391a321848c6ac9ab44933ea0da
Stathis Moraitidis <emoraitidis@gmail.com>
2019-02-03 11:54:14 +0000

    Add level images

--- src/scene/LevelMenuScene.js
+++ src/scene/LevelMenuScene.js
@@ -1,6 +1,6 @@
 import _LEVELS from '../../config/levels.json';
 
-const LEVELS_PER_ROW = 3;
+const LEVELS_PER_ROW = 4;
 const ROW_HEIGHT = 96;
 
 export default class LevelMenuScene extends Phaser.Scene {
@@ -13,23 +13,38 @@ export default class LevelMenuScene extends Phaser.Scene {
   create() {
     const config = this.sys.game.CONFIG;
     const levelWidthDistance = config.width / LEVELS_PER_ROW;
-    const levelPos = { x: levelWidthDistance, y: 96 };
-    const completedLevels =
-      JSON.parse(localStorage.getItem('completed-levels')) || [];
+    const levelPos = { x: levelWidthDistance, y: 128 };
+    const completedLevels = JSON.parse(
+      localStorage.getItem('completed-levels')
+    ) || [0];
     const nextLevel = Math.max(...completedLevels) + 1;
 
     for (let levelNumber = 1; levelNumber <= _LEVELS.length; levelNumber += 1) {
       const isCompleted = completedLevels.includes(levelNumber);
-      const colour = isCompleted ? 'green' : 'black';
+      const isNextLevel = levelNumber === nextLevel;
+
+      const colour = isCompleted ? '#1b8b1b' : 'black';
+      let imageKey = '';
+      if (isCompleted) {
+        imageKey = 'level_completed';
+      } else if (isNextLevel) {
+        imageKey = 'level_empty';
+      } else {
+        imageKey = 'level_locked';
+      }
+
+      const levelImage = this.add.image(0, 0, imageKey);
+      levelImage.x = levelPos.x - levelWidthDistance / 2;
+      levelImage.y = levelPos.y - levelImage.height;
 
       const levelText = this.add.text(0, 0, levelNumber.toString(), {
         fontFamily: 'Arial',
         fill: colour,
-        fontSize: 64,
+        fontSize: 32,
       });
       levelText.setPosition(
         levelPos.x - levelWidthDistance / 2 - levelText.width / 2,
-        levelPos.y - levelText.height
+        levelImage.y - levelImage.height / 2
       );
 
       if (levelNumber % LEVELS_PER_ROW === 0) {
@@ -40,15 +55,15 @@ export default class LevelMenuScene extends Phaser.Scene {
       }
 
       // Only allow click if it's completed or it's the next level
-      if (isCompleted || levelNumber === nextLevel || __DEV__) {
+      if (isCompleted || isNextLevel || __DEV__) {
         // Make the text interactive
-        levelText.setInteractive(
-          new Phaser.Geom.Rectangle(0, 0, levelText.width, levelText.height),
+        levelImage.setInteractive(
+          new Phaser.Geom.Rectangle(0, 0, levelImage.width, levelImage.height),
           Phaser.Geom.Rectangle.Contains
         );
 
         const { scene } = this;
-        levelText.on('pointerdown', () =>
+        levelImage.on('pointerdown', () =>
           scene.start('GameplayScene', { levelNumber })
         );
       }
