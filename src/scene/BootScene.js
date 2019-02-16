c6e402250d5b6a9df7c904f31f7767d429451dbf
Stathis Moraitidis <emoraitidis@gmail.com>
2019-02-16 23:41:17 +0000

    Add dummy retry image

--- src/scene/BootScene.js
+++ src/scene/BootScene.js
@@ -16,6 +16,7 @@ class BootScene extends Phaser.Scene {
     this.load.image('level_completed', 'assets/images/level_completed.png');
     this.load.image('level_locked', 'assets/images/level_locked.png');
     this.load.image('level_empty', 'assets/images/level_empty.png');
+    this.load.image('retry_popup', 'assets/images/retry_popup.png');
 
     this.load.audio('table_bounce', 'assets/sounds/table_bounce.mp3');
     this.load.audio('cup_bounce', 'assets/sounds/cup_bounce.mp3');
