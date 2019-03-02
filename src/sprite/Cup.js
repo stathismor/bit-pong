7fd8dd744acde69237b1615f32f35f867bd5c1ec
Stathis Moraitidis <emoraitidis@gmail.com>
2019-03-02 14:01:48 +0000

    Fix multiple cup collision sounds

--- src/sprite/Cup.js
+++ src/sprite/Cup.js
@@ -6,11 +6,13 @@ const SIDE_WITH = 10;
 const SIDES_ANGLE = 13;
 const OFFSET = 2;
 const CHAMFER_RADIUS = 7;
+const COLLISION_PERIOD = 200;
 
 export default class Cup extends Phaser.Physics.Matter.Sprite {
   constructor(scene, x, y, angleRad, ballId, behaviourName) {
     super(scene.matter.world, x, y, constants.TEXTURE_ATLAS, 'cup');
     this.behaviour = behaviour[behaviourName];
+    let collisionTime = new Date();
 
     // The player's body is going to be a compound body.
     const cupLeft = M.Bodies.rectangle(
@@ -46,13 +48,10 @@ export default class Cup extends Phaser.Physics.Matter.Sprite {
       .setStatic(true);
 
     const context = this;
-    scene.matter.world.on('collisionstart', event => {
+    scene.matter.world.on('collisionstart', (event, firstBodyA, firstBodyB) => {
       const { pairs } = event;
 
       for (let i = 0; i < pairs.length; i += 1) {
-        const { bodyA } = pairs[i];
-        const { bodyB } = pairs[i];
-
         //  We only want sensor collisions
         if (pairs[i].isSensor) {
           const currentLevel = scene.levelNumber;
@@ -65,12 +64,20 @@ export default class Cup extends Phaser.Physics.Matter.Sprite {
             );
           }
           scene.scene.start('LevelMenuScene');
-        } else if (
-          [bodyA.id, bodyB.id].includes(ballId) &&
-          [bodyA.id, bodyB.id].some(r => [cupLeft.id, cupRight.id].includes(r))
-        ) {
+        }
+      }
+
+      if (
+        [firstBodyA.id, firstBodyB.id].includes(ballId) &&
+        [firstBodyA.id, firstBodyB.id].some(r =>
+          [cupLeft.id, cupRight.id].includes(r)
+        )
+      ) {
+        const timeDiff = new Date() - collisionTime;
+        if (timeDiff > COLLISION_PERIOD) {
           context.scene.sound.play('cup_bounce');
         }
+        collisionTime = new Date();
       }
     });
   }
