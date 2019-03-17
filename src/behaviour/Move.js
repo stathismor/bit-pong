a96a8f3b75c5e8bf58f2740b54af045db55dae92
Stathis Moraitidis <emoraitidis@gmail.com>
2019-03-17 13:15:22 +0000

    Add splash

---
+++ src/behaviour/Move.js
@@ -0,0 +1,14 @@
+export default class Move {
+  constructor(scene, owner) {
+    scene.tweens.add({
+      targets: owner,
+      ease: 'Sine.easeInOut',
+      duration: 2100,
+      yoyo: true,
+      x: 550,
+      repeat: -1,
+    });
+  }
+
+  update() {}
+}
