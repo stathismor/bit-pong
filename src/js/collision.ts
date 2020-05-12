57b7c2b186f824d478637ac07f1d4008e6ce6519
Stathis <s.moraitidis@platomedia.com>
2020-05-12 23:14:52 +0100

    Fix matterjs not working after multiple restarts

--- src/js/collision.ts
+++ src/js/collision.ts
@@ -2,7 +2,8 @@ export let cupCategory = null;
 export let dropCategory = null;
 export let tableCategory = null;
 
-export function initCategories(scene) {
+export function initCategories(scene): void {
+  scene.matter.world.resetCollisionIDs();
   cupCategory = scene.matter.world.nextCategory();
   dropCategory = scene.matter.world.nextCategory();
   tableCategory = scene.matter.world.nextCategory();
