da0833078e688f88eed3a63d5a95281362895229
Stathis <s.moraitidis@platomedia.com>
2020-06-05 11:58:03 +0100

    Add a level

--- src/js/behaviour/Move.ts
+++ src/js/behaviour/Move.ts
@@ -1,14 +1,30 @@
 export class Move {
-  constructor(scene, owner) {
-    scene.tweens.add({
+  constructor(scene, owner, options) {
+    const tweenOptions = {
       targets: owner,
       ease: "Sine.easeInOut",
       duration: 2100,
       yoyo: true,
-      x: 550,
       repeat: -1,
-    });
+    };
+    let direction = "horizontal";
+
+    if (options) {
+      if (options.direction) {
+        direction = options.direction;
+      }
+
+      if (direction === "horizontal") {
+        if (options.x) {
+          tweenOptions["x"] = options.x;
+        } else {
+          tweenOptions["y"] = options.y;
+        }
+      }
+    }
+
+    scene.tweens.add(tweenOptions);
   }
 
-  update() {}
+  update(): void {}
 }
