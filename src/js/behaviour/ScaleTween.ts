6e7ca8b94fccacc3df8ec26fe8caf98c8bf91147
Stathis <emoraitidis@gmail.com>
2020-09-03 19:53:25 +0100

    Add one before last level

--- src/js/behaviour/ScaleTween.ts
+++ src/js/behaviour/ScaleTween.ts
@@ -1,14 +1,17 @@
 const DEFAULT_SCALE = 0.5;
+const DEFAULT_DURATION = 2100;
 
 export class ScaleTween {
   constructor(scene, owner, options) {
     const to = options && options.to ? options.to : DEFAULT_SCALE;
+    const duration =
+      options && options.duration ? options.duration : DEFAULT_DURATION;
 
     const tweenOptions = {
       targets: { to },
       key: { from: 1, to },
       ease: "Sine.easeInOut",
-      duration: 2100,
+      duration,
       yoyo: true,
       repeat: -1,
       onUpdate: function () {
